99c6062e3a585cd6e79285f2bd5ad1a6f6ab04cd
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2024-08-14 21:04:50 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    let newString = ''
+    if (num < 0) {
+        newString = 'ERROR';
+        return newString;
+    } else {
+        for (count = 0; count < num; count++) {
+        newString = newString.concat(string);
+        }
+    return newString;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
