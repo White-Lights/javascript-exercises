dfaa75c508a57ba27aa6ffdf8d35f9c7f9e03479
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2024-08-14 22:15:24 -0500

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString = '';
+    for (count = string.length; count >= 0; count--) {
+        revString = revString.concat(string.charAt(count))
+    }
+    return revString;
 };
 
 // Do not edit below this line
