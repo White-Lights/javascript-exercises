90b94566304502aadb7bc26ccbc297d28bcb4b23
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2024-08-15 20:15:39 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+function removeFromArray(testArray, ...itemsRemoved) {
+    let newArray = [ ];
+    for (const item of testArray) {
+        // console.log(itemsRemoved.indexOf(item))
+        if (itemsRemoved.indexOf(item) == -1) {
+            newArray.push(item);
+        }
+    }
+    console.log("testArray is: " + testArray);
+    console.log("Items to take out are :" + itemsRemoved);
+    console.log("newArray is :" + newArray);
+    return newArray;
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
