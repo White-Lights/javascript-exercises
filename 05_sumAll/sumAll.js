0030313b0302aae229d25fcc79afd0460bf35aab
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2024-08-15 21:31:15 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let total;
+    // All ERROR checks
+    if (firstNum < 0 ||
+        secondNum < 0 ||
+        Number.isInteger(firstNum) === false ||
+        Number.isInteger(secondNum) === false ) {
+        total = 'ERROR';
+    } else {
+        total = 0;
+    }
 
-};
+    // Assigning start and finish
+    if (firstNum < secondNum) {
+        start = firstNum;
+        finish = secondNum;
+    } else {
+        start = secondNum;
+        finish = firstNum;
+    }
+
+    // Commencing the loop
+    if(total == 'ERROR') {
+        return total;
+    } else {
+        for(count = start; count <= finish; count++) {
+            total += count;
+            }
+    }
+    console.log(`First number is ${firstNum}, second number is ${secondNum},\n
+        and your total is ${total}`)
+    return total;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
