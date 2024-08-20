8bd4e6a0a9e916e4706d6489a7b24634394b435a
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2024-08-19 21:01:22 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  let degreesCels = (degrees - 32) * (5/9);
+  degreesCels = (Math.round(degreesCels * 10)/10)
+  return degreesCels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  let degreesFahr = (degrees * (9/5) + 32);
+  degreesFahr = (Math.round(degreesFahr * 10)/10)
+  return degreesFahr;
 };
 
 // Do not edit below this line
