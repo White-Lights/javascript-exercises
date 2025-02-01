220c74838de8ded174850a4f82cfc8757781a07e
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2025-02-01 14:28:44 -0600

    Complete js exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    compare = string.split("")
+        // then mapped all into lower case via map
+        .map((i) => i.toLowerCase())
+        // then filtered for non alphanumeric characters
+        .filter((i) => 
+            i.charCodeAt() > 96 && i.charCodeAt() < 123 || //lowercase alphabet
+            i.charCodeAt() > 47 && i.charCodeAt() < 58); // 0-9
+    return compare.join("") == compare.reverse().join("");
+    // return compare.join("") == compare.reverse().join("");
 };
 
 // Do not edit below this line
