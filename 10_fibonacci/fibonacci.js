9d03e7964fc055beda71c9b6b1bf03aa074a5871
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2025-02-01 15:04:54 -0600

    Complete js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,9 @@
 const fibonacci = function(limit) {
     let array = []
+    
     if(limit == 0) { return 0 };
-    if(limit < 0) { return "OOPS" }
+    if(limit < 0) { return "OOPS" };
+
     for(i = 1; i <= limit; i++) {
         if(array.length < 2) {
             array.push(1);
