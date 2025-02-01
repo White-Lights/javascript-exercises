36ab5672d764d3508f7ec5aca12af0ceeba30334
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2025-02-01 13:33:05 -0600

    Finish js exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(...args) {
+  return args.reduce((total, current) => total += current);
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args.reduce((total, current) => total -= current);
 };
 
-const sum = function() {
-	
+const sum = function(arg) {
+  return arg.length !== 0 ? 
+    arg.reduce((total, current) => total += current):
+    0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, current) => total *= current);
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	return num ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
