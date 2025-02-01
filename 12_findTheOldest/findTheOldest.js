fdd5f1358a2d29e3de169961468109b81bb57d0e
Whitney Bean <143766355+White-Lights@users.noreply.github.com>
2025-02-01 15:58:46 -0600

    Complete js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach((person) => {
+        if(person.yearOfDeath == undefined) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth;
+    })
+    people.sort(compare);
+    return people[0];
 };
+
+function compare(a, b) {
+    if (a.age < b.age) return 1; // if the first value is less than the second
+    if (a.age == b.age) return 0; // if values are equal
+    if (a.age > b.age) return -1; // if the first value is greater than the second
+  }
 
 // Do not edit below this line
 module.exports = findTheOldest;
